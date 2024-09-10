f6f852b04e8f35af59db4274146c37b70d30c5f3
Ahmad Karim <ahmadkarimarbab@gmail.com>
2024-09-10 22:04:45 +0500

    video_feature_tracking1.py

--- video_feature_tracking1.py
+++ video_feature_tracking1.py
@@ -11,15 +11,15 @@ else:
         ret, frame = cap.read()
         if not ret or frame is None:
             break
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        corners = cv2.goodFeaturesToTrack(gray_frame, maxCorners=100, qualityLevel=0.01, minDistance=10)
-        if corners is not None:
-            corners = np.int32(corners)  # Convert corners to integer
-            for corner in corners:
-                x, y = corner.ravel()
-                cv2.rectangle(frame, (x - 10, y - 10), (x + 10, y + 10), (0, 255, 0), 2)
-        cv2.imshow('Frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.desttroyAllWindows()
+    #     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #     corners = cv2.goodFeaturesToTrack(gray_frame, maxCorners=100, qualityLevel=0.01, minDistance=10)
+    #     if corners is not None:
+    #         corners = np.int32(corners)  # Convert corners to integer
+    #         for corner in corners:
+    #             x, y = corner.ravel()
+    #             cv2.rectangle(frame, (x - 10, y - 10), (x + 10, y + 10), (0, 255, 0), 2)
+    #     cv2.imshow('Frame', frame)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    # cap.release()
+    # cv2.desttroyAllWindows()
